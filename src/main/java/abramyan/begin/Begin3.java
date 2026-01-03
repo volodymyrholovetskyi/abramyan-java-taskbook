1ebfc5ff32a20d62255cdd6209c7a3c2fc57942c
Volodymyr Holovetskyi <vholovetskyi@gmail.com>
2026-01-03 14:37:46 +0100

    Added Begin3 solution

---
+++ src/main/java/abramyan/begin/Begin3.java
@@ -0,0 +1,46 @@
+package abramyan.begin;
+
+import static abramyan.utils.InputReader.nextInt;
+
+/**
+ * Abramyan – Begin3
+ * <p>
+ * Problem:
+ * Given the sides of a rectangle a and b.
+ * Find its area and perimeter:
+ * S = a * b
+ * P = 2 * (a + b)
+ * <p>
+ * Input:
+ * Two integer values a and b
+ * <p>
+ * Output:
+ * The area and the perimeter of the rectangle
+ * <p>
+ * Hint:
+ * - Read two input values
+ * - Calculate area first, then perimeter
+ * <p>
+ * Complexity:
+ * O(1)
+ * <p>
+ * Author:
+ * Volodymyr Holovetskyi
+ */
+public class Begin3 {
+
+    public static void main(String[] args) {
+        int a = nextInt();
+        int b = nextInt();
+        System.out.println(calculateArea(a, b));
+        System.out.println(calculatePerimeter(a, b));
+    }
+
+    private static int calculateArea(int a, int b) {
+        return a * b;
+    }
+
+    private static int calculatePerimeter(int a, int b) {
+        return 2 * (a + b);
+    }
+}
