f77b36140afd3f1261779f60a5b6b90f71e1f001
Volodymyr Holovetskyi <volodymyrgolovetskyi1995@gmail.com>
2026-01-04 21:21:52 +0100

    Added Begin4 class

---
+++ src/main/java/abramyan/begin/Begin4.java
@@ -0,0 +1,36 @@
+package abramyan.begin;
+
+/**
+ * Abramyan – Begin4
+ *
+ * Problem:
+ * Given the diameter of a circle d.
+ * Find its circumference:
+ * L = π * d
+ * Use π = 3.14
+ *
+ * Input:
+ * One integer value d
+ *
+ * Output:
+ * The circumference of the circle
+ *
+ * Hint:
+ * - Define a constant value for PI
+ * - Multiply PI by the diameter
+ *
+ * Complexity:
+ * O(1)
+ *
+ * Author:
+ * Your Name
+ */
+public class Begin4 {
+
+    public static void main(String[] args) {
+        // TODO: read input value d
+        // TODO: define PI constant
+        // TODO: calculate circumference
+        // TODO: print result
+    }
+}
