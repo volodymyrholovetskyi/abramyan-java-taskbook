e00683c1317786eb14653247e9fe3a747282078b
Volodymyr Holovetskyi <volodymyrgolovetskyi1995@gmail.com>
2026-01-04 21:28:56 +0100

    Added Begin4 solution

--- src/main/java/abramyan/begin/Begin4.java
+++ src/main/java/abramyan/begin/Begin4.java
@@ -1,36 +1,42 @@
 package abramyan.begin;
 
+import static abramyan.utils.InputReader.nextInt;
+
 /**
  * Abramyan – Begin4
- *
+ * <p>
  * Problem:
  * Given the diameter of a circle d.
  * Find its circumference:
  * L = π * d
  * Use π = 3.14
- *
+ * <p>
  * Input:
  * One integer value d
- *
+ * <p>
  * Output:
  * The circumference of the circle
- *
+ * <p>
  * Hint:
  * - Define a constant value for PI
  * - Multiply PI by the diameter
- *
+ * <p>
  * Complexity:
  * O(1)
- *
+ * <p>
  * Author:
  * Your Name
  */
 public class Begin4 {
 
+    private static final double PI = 3.14;
+
     public static void main(String[] args) {
-        // TODO: read input value d
-        // TODO: define PI constant
-        // TODO: calculate circumference
-        // TODO: print result
+        int d = nextInt();
+        System.out.println(calculateCircumference(d));
+    }
+
+    private static double calculateCircumference(int d) {
+        return PI * d;
     }
 }
