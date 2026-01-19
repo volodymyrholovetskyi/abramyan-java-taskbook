9a44ada6d8c1cfd17c4bd01f2bc20351e6cbe1fb
Volodymyr Holovetskyi <volodymyrgolovetskyi1995@gmail.com>
2026-01-19 22:09:46 +0100

    Added Begin34 class

---
+++ src/main/java/abramyan/begin/Begin34.java
@@ -0,0 +1,52 @@
+package abramyan.begin;
+
+import static abramyan.utils.InputReader.nextDouble;
+
+/**
+ * Abramyan – Begin34
+ *
+ * Problem:
+ * It is known that:
+ * - X kilograms of chocolate candies cost A rubles
+ * - Y kilograms of toffee cost B rubles
+ *
+ * Find:
+ * - the cost of 1 kg of chocolate candies
+ * - the cost of 1 kg of toffee
+ * - how many times chocolate candies are more expensive than toffee
+ *
+ * Input:
+ * Four double values:
+ * X, A, Y, B
+ *
+ * Output:
+ * - Cost of 1 kg of chocolate candies
+ * - Cost of 1 kg of toffee
+ * - Price ratio (chocolate / toffee)
+ *
+ * Hint:
+ * - Calculate unit prices first
+ *
+ * Complexity:
+ * O(1)
+ *
+ * Author:
+ * Your Name
+ */
+public class Begin34 {
+
+    public static void main(String[] args) {
+        double x = nextDouble();
+        double a = nextDouble();
+        double y = nextDouble();
+        double b = nextDouble();
+
+        // TODO: calculate price per kg of chocolate candies
+
+        // TODO: calculate price per kg of toffee
+
+        // TODO: calculate price ratio
+
+        // TODO: print results
+    }
+}
