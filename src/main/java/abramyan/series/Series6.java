2a25473969c860334136d0ee28c8055e0ce8ab45
Volodymyr Holovetskyi <volodymyrgolovetskyi1995@gmail.com>
2026-01-29 21:36:59 +0100

    Added Series6 class

---
+++ src/main/java/abramyan/series/Series6.java
@@ -0,0 +1,43 @@
+package abramyan.series;
+
+import static abramyan.utils.InputReader.nextInt;
+import static abramyan.utils.InputReader.nextDouble;
+
+/**
+ * Abramyan – Series6
+ *
+ * Problem:
+ * Given an integer N and a sequence of N positive real numbers.
+ * Output the fractional parts of the numbers (as doubles)
+ * and the product of all fractional parts.
+ *
+ * Input:
+ * Integer N, followed by N positive double values
+ *
+ * Output:
+ * - Fractional parts in the same order
+ * - Product of fractional parts
+ *
+ * Hint:
+ * - Fractional part = number - integer part
+ * - Initialize product correctly
+ *
+ * Complexity:
+ * O(N)
+ *
+ * Author:
+ * Your Name
+ */
+public class Series6 {
+
+    public static void main(String[] args) {
+        int n = nextInt();
+
+        // TODO: initialize product accumulator
+        // TODO: read N numbers in a loop
+        // TODO: extract fractional part
+        // TODO: print fractional part
+        // TODO: update product
+        // TODO: print product after loop
+    }
+}
