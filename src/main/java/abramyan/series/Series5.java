565e27793e9e32608ff0d2b3d99199e336ecf573
Volodymyr Holovetskyi <volodymyrgolovetskyi1995@gmail.com>
2026-01-29 21:24:21 +0100

    Added Series5 class

---
+++ src/main/java/abramyan/series/Series5.java
@@ -0,0 +1,43 @@
+package abramyan.series;
+
+import static abramyan.utils.InputReader.nextInt;
+import static abramyan.utils.InputReader.nextDouble;
+
+/**
+ * Abramyan – Series5
+ *
+ * Problem:
+ * Given an integer N and a sequence of N positive real numbers.
+ * Output the integer parts of the numbers (as doubles)
+ * and the sum of all integer parts.
+ *
+ * Input:
+ * Integer N, followed by N positive double values
+ *
+ * Output:
+ * - Integer parts in the same order
+ * - Sum of integer parts
+ *
+ * Hint:
+ * - Use casting or Math.floor
+ * - Preserve input order
+ *
+ * Complexity:
+ * O(N)
+ *
+ * Author:
+ * Your Name
+ */
+public class Series5 {
+
+    public static void main(String[] args) {
+        int n = nextInt();
+
+        // TODO: initialize sum accumulator
+        // TODO: read N numbers in a loop
+        // TODO: extract integer part
+        // TODO: print integer part
+        // TODO: update sum
+        // TODO: print sum after loop
+    }
+}
